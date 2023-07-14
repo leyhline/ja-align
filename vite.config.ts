165c51fe0c8c1d73729b64df79267810c4000ce8
Thomas Leyh <thomas.leyh@mailbox.org>
2023-07-14 23:00:25 +0200

    use service worker to cache everything

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,58 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { UserConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 const prodConfig: UserConfig = {
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      includeAssets: [
+        'favicon.ico',
+        'logo.svg',
+        'apple-touch-icon-180x180.png',
+        'maskable-icon-512x512.png',
+        'pwa-64x64.png',
+        'pwa-192x192.png',
+        'pwa-512x512.png'
+      ],
+      manifest: {
+        name: 'Align Japanese TTS',
+        short_name: 'ja-align',
+        description: 'Align Japanese Text and Audio',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      },
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,json,ogg,txt,woff2,zip,gz,wasm}'],
+        maximumFileSizeToCacheInBytes: 100000000
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
